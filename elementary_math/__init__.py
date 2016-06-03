0644a2e5e189bd8ca29e837b9e4c52435ed3dce4
MacHu-GWU <husanhe@gmail.com>
2016-06-03 12:35:47 -0400

    2016-06-03

--- elementary_math/__init__.py
+++ elementary_math/__init__.py
@@ -5,6 +5,6 @@
 Elementary Math Library. (For demonstration purpose only) 
 """
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __short_description__ = "Elementary Mathematics."
 __license__ = "MIT"
