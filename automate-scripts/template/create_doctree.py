663b0e892f30a941b4f53ab195ddae0cff365fb3
MacHu-GWU <husanhe@gmail.com>
2016-06-16 17:59:02 -0400

    2016-06-16

--- automate-scripts/template/create_doctree.py
+++ automate-scripts/template/create_doctree.py
@@ -6,7 +6,7 @@ import docfly
 # Uncomment this if you follow Sanhe's Sphinx Doc Style Guide
 #--- Manually Made Doc ---
 # doc = docfly.DocTree("source")
-# doc.fly()
+# doc.fly(table_of_content_header="Table of Content (目录)")
 
 #--- Api Reference Doc ---
 package_name = "{{ package_name }}"
